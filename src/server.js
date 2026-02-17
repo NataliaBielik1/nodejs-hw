a0659ccc2dffc7b33113e8125324a7e6f32605d1
Nataliia Bielik <natalia.belic2607@gmail.com>
2026-02-16 20:07:30 -0500

    fixed mistakes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 import cors from "cors";
 import express from "express";
-import pino from 'pino';
+import pinoHttp from 'pino-http';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(
-  pino({
+  pinoHttp({
     level: 'info',
     transport: {
       target: 'pino-pretty',
@@ -28,15 +28,16 @@ app.get("/notes", (request, response) => {
 });
 
 app.get("/notes/:noteId", (request, response) => {
-  response.json({ message: "Retrieved note with ID: id_param" });
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({ "message": "Route not found" });
+  const { noteId } = request.params;
+  response.json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
 app.get('/test-error', (request, response) => {
   throw new Error('Simulated server error');
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ "message": "Route not found" });
 });
 
 app.use((error, req, res, next) => {
@@ -45,13 +46,13 @@ app.use((error, req, res, next) => {
 });
 
 
-const PORT = Number.parseInt(process.env.PORT, 10) || 8080;
+const PORT = Number.parseInt(process.env.PORT, 10) || 3000;
 
 app.listen(PORT, error => {
   if (error) {
     throw error;
   }
-  console.log("Server started on port ${PORT}");
+  console.log(`Server started on port ${PORT}`);
 });
 
 
